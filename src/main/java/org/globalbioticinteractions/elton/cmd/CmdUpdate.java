f045d7c6519869037e60d81d8238d406ab27d6c3
jhpoelen <jhpoelen@xs4all.nl>
2017-09-20 15:50:48 -0700

    added logging

--- src/main/java/org/globalbioticinteractions/elton/cmd/CmdUpdate.java
+++ src/main/java/org/globalbioticinteractions/elton/cmd/CmdUpdate.java
@@ -1,6 +1,8 @@
 package org.globalbioticinteractions.elton.cmd;
 
 import com.beust.jcommander.Parameters;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eol.globi.service.Dataset;
 import org.eol.globi.service.DatasetFactory;
 import org.eol.globi.service.DatasetFinder;
@@ -16,11 +18,14 @@ import java.util.Arrays;
 @Parameters(separators = "= ", commandDescription = "Update Datasets with Local Repository")
 public class CmdUpdate extends CmdDefaultParams {
 
+    private final static Log LOG = LogFactory.getLog(CmdUpdate.class);
+
     @Override
     public void run() {
         DatasetFinder finder = new DatasetFinderProxy(Arrays.asList(new DatasetFinderZenodo(), new DatasetFinderGitHubArchive()));
         try {
             NamespaceHandler handler = namespace -> {
+                LOG.info("update of [" + namespace + "] starting...");
                 Dataset dataset =
                         DatasetFactory.datasetFor(namespace, new DatasetFinderCaching(finder, getCacheDir()));
                 NodeFactoryNull nodeFactoryNull = new NodeFactoryNull();
@@ -28,8 +33,11 @@ public class CmdUpdate extends CmdDefaultParams {
                 new GitHubImporterFactory()
                         .createImporter(dataset, nodeFactoryNull)
                         .importStudy();
+                LOG.info("update of [" + namespace + "] done.");
             };
+            LOG.info("updates starting...");
             CmdUtil.handleNamespaces(finder, handler, getNamespaces());
+            LOG.info("updates done.");
         } catch (DatasetFinderException e) {
             throw new RuntimeException(e);
         }

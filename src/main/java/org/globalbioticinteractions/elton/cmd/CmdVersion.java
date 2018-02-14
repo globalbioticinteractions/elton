85b7bb2738a9c60b8a77ca667607160c71e0ed4a
jhpoelen <jhpoelen@xs4all.nl>
2018-02-13 16:12:17 -0800

    extract version using package api

--- src/main/java/org/globalbioticinteractions/elton/cmd/CmdVersion.java
+++ src/main/java/org/globalbioticinteractions/elton/cmd/CmdVersion.java
@@ -1,13 +1,25 @@
 package org.globalbioticinteractions.elton.cmd;
 
 import com.beust.jcommander.Parameters;
+import com.jcabi.manifests.Manifests;
+import org.apache.commons.lang.StringUtils;
 import org.eol.globi.Version;
+import org.globalbioticinteractions.elton.Elton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 @Parameters(separators = "= ", commandDescription = "Show Version")
 public class CmdVersion implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(Version.getVersion());
+        System.out.println(CmdVersion.class.getPackage().getImplementationVersion());
     }
+
 }

87f2bdc24e780139c6790e22a16d7b3ee506382e
jhpoelen <jhpoelen@xs4all.nl>
2020-06-22 13:01:31 -0700

    towards an elton init command

---
+++ src/main/java/org/globalbioticinteractions/elton/cmd/CmdInit.java
@@ -0,0 +1,196 @@
+package org.globalbioticinteractions.elton.cmd;
+
+import com.Ostermiller.util.LabeledCSVParser;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
+import org.eol.globi.util.CSVTSVUtil;
+import org.eol.globi.util.ResourceUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Parameters(separators = "= ", commandDescription = "Initialize a GloBI indexed dataset configuration")
+public class CmdInit extends CmdDefaultParams {
+
+    @Parameter(names = {"--data-url"}, description = "data url", required = true)
+    private String dataUrl;
+
+    @Parameter(names = {"--data-citation"}, description = "data citation", required = true)
+    private String dataCitation;
+
+    static String generateReadme(String citation, String namespace) {
+        return "[![Build Status](https://travis-ci.com/" + namespace + ".svg)](https://travis-ci.com/" + namespace + ") [![GloBI](http://api.globalbioticinteractions.org/interaction.svg?accordingTo=globi:" + namespace + ")](http://globalbioticinteractions.org/?accordingTo=globi:" + namespace + ")\n" +
+                "\n" +
+                "Configuration to help make: \n\n" +
+                citation +
+                "\navailable through Global Biotic Interactions (GloBI, https://globalbioticinteractions.org).";
+    }
+
+    static List<String> firstTwoLines(String urlString) throws IOException {
+        InputStream inputStream = ResourceUtil.asInputStream(urlString, in -> in);
+
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String candidateHeader = inputStreamReader.readLine();
+        String candidateValues = inputStreamReader.readLine();
+
+        return Arrays.asList(candidateHeader, candidateValues);
+    }
+
+    static List<String> inferColumnNamesTSV(List<String> firstTwoLines) {
+        List<String> columnNames;
+        if (firstTwoLines.size() < 2) {
+            columnNames = Collections.emptyList();
+        } else {
+            String[] header = CSVTSVUtil.splitTSV(firstTwoLines.get(0));
+            String[] values = CSVTSVUtil.splitTSV(firstTwoLines.get(1));
+            columnNames = header != null
+                    && values != null
+                    && header.length == values.length
+                    && header.length > 1
+                    ? Arrays.asList(header)
+                    : Collections.emptyList();
+        }
+        return columnNames;
+    }
+
+    static String generateConfig(String urlString, String citation) throws IOException {
+        List<String> firstTwoLines = firstTwoLines(urlString);
+
+        String actualConfig;
+        List<String> columnNames = extractColumnNamesCSV(firstTwoLines);
+        if (columnNames.size() > 0) {
+            actualConfig = generateConfig(urlString, citation, columnNames, ",");
+        } else {
+            columnNames = inferColumnNamesTSV(firstTwoLines);
+            if (columnNames.size() > 0) {
+                actualConfig = generateConfig(urlString, citation, columnNames, "\t");
+            } else {
+                ObjectMapper objectMapper = new ObjectMapper();
+                ObjectNode node = objectMapper.createObjectNode();
+                node.put("format", "unknown");
+                node.put("citation", citation);
+                node.put("url", urlString);
+                actualConfig = node.toString();
+            }
+        }
+        return actualConfig;
+    }
+
+    private static String generateConfig(String urlString, String citation, List<String> columnNames, String delimiter) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("@context", mapper.readTree("[\"http://www.w3.org/ns/csvw\", {\"@language\": \"en\"}]"));
+        objectNode.put("rdfs:comment", mapper.readTree("[\"inspired by https://www.w3.org/TR/2015/REC-tabular-data-model-20151217/\"]"));
+        objectNode.put("url", urlString);
+        objectNode.put("dcterms:bibliographicCitation", citation);
+        objectNode.put("delimiter", delimiter);
+        objectNode.put("headerRowCount", 1);
+        objectNode.put("null", mapper.readTree("[\"\"]"));
+        objectNode.put("tableSchema", generateColumns(columnNames));
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectNode);
+    }
+
+    private static List<String> extractColumnNamesCSV(List<String> firstTwoLines) throws IOException {
+        return extractColumnNamesCSV(IOUtils.toInputStream(StringUtils.join(firstTwoLines, "\n"), StandardCharsets.UTF_8));
+    }
+
+    private static JsonNode generateColumns(List<String> columnNames) {
+        ArrayNode arrayNode = new ObjectMapper().createArrayNode();
+
+        columnNames
+                .stream()
+                .map(StringUtils::trim)
+                .map(headerName -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    ObjectNode objectNode = objectMapper.createObjectNode();
+                    objectNode.put("name", headerName);
+                    objectNode.put("titles", headerName);
+                    objectNode.put("datatype", "string");
+                    return objectNode;
+                })
+                .forEach(arrayNode::add);
+
+        ObjectNode objectNode = new ObjectMapper().createObjectNode();
+        objectNode.put("columns", arrayNode);
+        return objectNode;
+    }
+
+    private static List<String> extractColumnNamesCSV(InputStream inputStream) throws IOException {
+        List<String> columnNames;
+        try {
+            LabeledCSVParser csvParser = new LabeledCSVParser(CSVTSVUtil.createCSVParser(inputStream));
+            columnNames = csvParser.getLabels().length > 1
+                    ? new ArrayList<>(Arrays.asList(csvParser.getLabels()))
+                    : Collections.emptyList();
+        } catch (IOException ex) {
+            columnNames = Collections.emptyList();
+        }
+        return columnNames;
+    }
+
+    @Override
+    public void run() {
+        for (String namespace : getNamespaces()) {
+            try {
+                write(generateReadme(getDataCitation(), namespace), "README.md");
+                write(generateConfig(getDataUrl(), getDataCitation()), "globi.json");
+                InputStreamFactoryLogging inputStreamFactory = createInputStreamFactory();
+                InputStream travis = ResourceUtil.asInputStream(URI.create("classpath:/org/globalbioticinteractions/elton/template/.travis.yml"), inputStreamFactory);
+                IOUtils.copy(travis, getFileOutputStream(".travis.yml"));
+                InputStream gitIgnore = ResourceUtil.asInputStream(URI.create("classpath:/org/globalbioticinteractions/elton/template/.gitignore"), inputStreamFactory);
+                IOUtils.copy(gitIgnore, getFileOutputStream(".gitignore"));
+                IOUtils.copy(gitIgnore, getFileOutputStream(".gitignore"));
+            } catch (IOException e) {
+                throw new RuntimeException("failed to initialize [" + namespace + "]", e);
+            }
+        }
+
+    }
+
+    private void write(String content, String filename) throws IOException {
+        IOUtils.copy(IOUtils.toInputStream(content, StandardCharsets.UTF_8),
+                getFileOutputStream(filename));
+    }
+
+    private FileOutputStream getFileOutputStream(String filename) throws FileNotFoundException {
+        return new FileOutputStream(new File(new File(getWorkDir()), filename));
+    }
+
+    public void setDataUrl(String dataUrl) {
+        this.dataUrl = dataUrl;
+    }
+
+    public String getDataUrl() {
+        return dataUrl;
+    }
+
+    public void setDataCitation(String dataCitation) {
+        this.dataCitation = dataCitation;
+    }
+
+    public String getDataCitation() {
+        return dataCitation;
+    }
+}
---
+++ src/test/java/org/globalbioticinteractions/elton/cmd/CmdInitTest.java
@@ -0,0 +1,183 @@
+package org.globalbioticinteractions.elton.cmd;
+
+import com.Ostermiller.util.LabeledCSVParser;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.eol.globi.util.CSVTSVUtil;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+public class CmdInitTest {
+
+    @Test
+    public void gatherInput() throws IOException {
+        File tempFile = File.createTempFile("init", "test", new File("target"));
+        FileUtils.forceDelete(tempFile);
+        FileUtils.forceMkdir(tempFile);
+
+        CmdInit cmdInit = new CmdInit();
+        cmdInit.setDataUrl("classpath:/org/globalbioticinteractions/elton/cmd/data.csv");
+        cmdInit.setDataCitation("some citation");
+        cmdInit.setWorkDir(tempFile.getAbsolutePath());
+        cmdInit.getNamespaces().add("some/namespace");
+
+        assertFalse(new File(tempFile, "README.md").exists());
+        assertFalse(new File(tempFile, "globi.json").exists());
+        assertFalse(new File(tempFile, ".travis.yaml").exists());
+        assertFalse(new File(tempFile, ".gitignore").exists());
+        cmdInit.run();
+
+        assertTrue(new File(tempFile, "README.md").exists());
+        assertTrue(new File(tempFile, "globi.json").exists());
+        assertTrue(new File(tempFile, ".travis.yml").exists());
+        assertTrue(new File(tempFile, ".gitignore").exists());
+    }
+
+    @Test
+    public void generateREADME() {
+        String citation = "the parasite-host records in collection of the University of Michigan Museum of Zoology Division of Insects";
+        String namespace = "globalbioticinteractions/ummzi";
+
+        String expectedReadme = "[![Build Status](https://travis-ci.com/globalbioticinteractions/ummzi.svg)](https://travis-ci.com/globalbioticinteractions/ummzi) [![GloBI](http://api.globalbioticinteractions.org/interaction.svg?accordingTo=globi:globalbioticinteractions/ummzi)](http://globalbioticinteractions.org/?accordingTo=globi:globalbioticinteractions/ummzi)\n" +
+                "\n" +
+                "Configuration to help make: \n\nthe parasite-host records in collection of the University of Michigan Museum of Zoology Division of Insects" +
+                "\navailable through Global Biotic Interactions (GloBI, https://globalbioticinteractions.org).";
+
+        String actualReadme = CmdInit.generateReadme(citation, namespace);
+
+        assertThat(actualReadme, Is.is(expectedReadme));
+    }
+
+    private List<String> inferColumnNamesCSV(InputStream is) throws IOException {
+        List<String> columnNames;
+        try {
+            LabeledCSVParser csvParser = new LabeledCSVParser(CSVTSVUtil.createCSVParser(is));
+            columnNames = csvParser.getLabels().length > 1
+                    ? new ArrayList<>(Arrays.asList(csvParser.getLabels()))
+                    : Collections.emptyList();
+        } catch (IOException ex) {
+            columnNames = Collections.emptyList();
+        }
+        return columnNames;
+    }
+
+    @Test
+    public void inferColumnNamesCSV() throws IOException {
+        String urlString = "classpath:/org/globalbioticinteractions/elton/cmd/data.csv";
+        List<String> firstTwoLines = CmdInit.firstTwoLines(urlString);
+        List<String> columnNames = inferColumnNamesCSV(IOUtils.toInputStream(StringUtils.join(firstTwoLines, "\n"), StandardCharsets.UTF_8));
+
+        assertEquals(columnNames, Arrays.asList("header1", "header2", "header3"));
+    }
+
+    @Test
+    public void inferColumnNamesTSV() throws IOException {
+        String urlString = "classpath:/org/globalbioticinteractions/elton/cmd/data.tsv";
+        List<String> columnNames = CmdInit.inferColumnNamesTSV(CmdInit.firstTwoLines(urlString));
+
+        assertEquals(columnNames, Arrays.asList("header1", "header2", "header3"));
+
+
+    }
+
+    @Test
+    public void inferDefaultSchemaFromCSV() throws IOException {
+
+        String expectedConfig = "{\n" +
+                "  \"@context\" : [ \"http://www.w3.org/ns/csvw\", {\n" +
+                "    \"@language\" : \"en\"\n" +
+                "  } ],\n" +
+                "  \"rdfs:comment\" : [ \"inspired by https://www.w3.org/TR/2015/REC-tabular-data-model-20151217/\" ],\n" +
+                "  \"url\" : \"classpath:/org/globalbioticinteractions/elton/cmd/data.csv\",\n" +
+                "  \"dcterms:bibliographicCitation\" : \"some citation\",\n" +
+                "  \"delimiter\" : \",\",\n" +
+                "  \"headerRowCount\" : 1,\n" +
+                "  \"null\" : [ \"\" ],\n" +
+                "  \"tableSchema\" : {\n" +
+                "    \"columns\" : [ {\n" +
+                "      \"name\" : \"header1\",\n" +
+                "      \"titles\" : \"header1\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"header2\",\n" +
+                "      \"titles\" : \"header2\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"header3\",\n" +
+                "      \"titles\" : \"header3\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    } ]\n" +
+                "  }\n" +
+                "}";
+
+        String urlString = "classpath:/org/globalbioticinteractions/elton/cmd/data.csv";
+        String citation = "some citation";
+
+        String actualConfig = CmdInit.generateConfig(urlString, citation);
+        assertThat(actualConfig, Is.is(expectedConfig));
+    }
+
+    @Test
+    public void inferDefaultSchemaFromTSV() throws IOException {
+
+        String expectedConfig = "{\n" +
+                "  \"@context\" : [ \"http://www.w3.org/ns/csvw\", {\n" +
+                "    \"@language\" : \"en\"\n" +
+                "  } ],\n" +
+                "  \"rdfs:comment\" : [ \"inspired by https://www.w3.org/TR/2015/REC-tabular-data-model-20151217/\" ],\n" +
+                "  \"url\" : \"classpath:/org/globalbioticinteractions/elton/cmd/data.tsv\",\n" +
+                "  \"dcterms:bibliographicCitation\" : \"some citation\",\n" +
+                "  \"delimiter\" : \"\\t\",\n" +
+                "  \"headerRowCount\" : 1,\n" +
+                "  \"null\" : [ \"\" ],\n" +
+                "  \"tableSchema\" : {\n" +
+                "    \"columns\" : [ {\n" +
+                "      \"name\" : \"header1\",\n" +
+                "      \"titles\" : \"header1\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"header2\",\n" +
+                "      \"titles\" : \"header2\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    }, {\n" +
+                "      \"name\" : \"header3\",\n" +
+                "      \"titles\" : \"header3\",\n" +
+                "      \"datatype\" : \"string\"\n" +
+                "    } ]\n" +
+                "  }\n" +
+                "}";
+
+        String urlString = "classpath:/org/globalbioticinteractions/elton/cmd/data.tsv";
+        String citation = "some citation";
+
+        String actualConfig = CmdInit.generateConfig(urlString, citation);
+        assertThat(actualConfig, Is.is(expectedConfig));
+    }
+
+    @Test
+    public void inferDefaultSchemaFromNonTSVCSV() throws IOException {
+
+        String urlString = "classpath:/org/globalbioticinteractions/elton/cmd/data.txt";
+        String citation = "some citation";
+
+        String actualConfig = CmdInit.generateConfig(urlString, citation);
+        assertThat(actualConfig, Is.is("{\"format\":\"unknown\",\"citation\":\"some citation\",\"url\":\"classpath:/org/globalbioticinteractions/elton/cmd/data.txt\"}"));
+    }
+
+}

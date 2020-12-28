89e56e82cafc8af29e67209d5f89634d1da4855a
jhpoelen <jhpoelen@xs4all.nl>
2020-12-28 11:30:13 -0800

    change final with excluded version string

--- .travis.deploy_if_tag.sh
+++ .travis.deploy_if_tag.sh
@@ -2,6 +2,6 @@
 if [ -n "$TRAVIS_TAG" ]; then
   mvn -s .travis.maven.settings.xml -DskipTests clean deploy
   VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cp target/elton-${VERSION}-jar-with-dependencies.jar elton_no_head.jar
+  cp target/elton-app.jar elton_no_head.jar
   cat .travis.jar.magic elton_no_head.jar > elton.jar
 fi

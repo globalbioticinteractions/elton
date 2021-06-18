ceb2ac1f7cd80f56e8f0ec67e68e7061c055a5c2
jhpoelen <jhpoelen@xs4all.nl>
2021-06-18 16:04:37 -0700

    first integration for elton with preston-cas; related to https://github.com/bio-guoda/preston/issues/127

---
+++ src/main/java/org/globalbioticinteractions/elton/store/CachePullThroughPrestonStore.java
@@ -0,0 +1,74 @@
+package org.globalbioticinteractions.elton.store;
+
+import bio.guoda.preston.model.RefNodeFactory;
+import bio.guoda.preston.store.BlobStoreAppendOnly;
+import bio.guoda.preston.store.Dereferencer;
+import bio.guoda.preston.store.DereferencerContentAddressed;
+import bio.guoda.preston.store.KeyTo1LevelPath;
+import bio.guoda.preston.store.KeyValueStoreLocalFileSystem;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.rdf.api.IRI;
+import org.eol.globi.util.DateUtil;
+import org.eol.globi.util.InputStreamFactory;
+import org.eol.globi.util.ResourceUtil;
+import org.globalbioticinteractions.cache.CachePullThrough;
+import org.globalbioticinteractions.cache.CacheUtil;
+import org.globalbioticinteractions.cache.ContentProvenance;
+import org.globalbioticinteractions.cache.ProvenanceLog;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class CachePullThroughPrestonStore extends CachePullThrough {
+
+    private final String namespace;
+    private final String cachePath;
+    private final InputStreamFactory inputStreamFactory;
+
+    public CachePullThroughPrestonStore(String namespace, String cachePath, InputStreamFactory inputStreamFactory) {
+        super(namespace, cachePath, inputStreamFactory);
+        this.namespace = namespace;
+        this.cachePath = cachePath;
+        this.inputStreamFactory = inputStreamFactory;
+    }
+
+    public InputStream retrieve(URI resourceURI) throws IOException {
+        CacheUtil.findOrMakeCacheDirForNamespace(cachePath, namespace);
+
+        File cacheDir = new File(cachePath, namespace);
+        KeyTo1LevelPath keyToPath = new KeyTo1LevelPath(cacheDir.toURI());
+        BlobStoreAppendOnly blobStore = new BlobStoreAppendOnly(
+                new KeyValueStoreLocalFileSystem(
+                        cacheDir,
+                        keyToPath,
+                        new KeyValueStoreLocalFileSystem.KeyValueStreamFactorySHA256Values()
+                )
+        );
+
+        Dereferencer<InputStream> deref
+                = iri -> ResourceUtil.asInputStream(iri.getIRIString(), inputStreamFactory);
+
+        DereferencerContentAddressed derefCas = new DereferencerContentAddressed(deref, blobStore);
+
+
+        IRI dereferenced = derefCas.dereference(RefNodeFactory.toIRI(resourceURI));
+
+        URI localPathURI = keyToPath.toPath(dereferenced);
+        ContentProvenance contentProvenanceWithNamespace
+                = new ContentProvenance(namespace,
+                resourceURI,
+                localPathURI,
+                StringUtils.replace(dereferenced.getIRIString(), "hash://sha256/", ""),
+                DateUtil.nowDateString());
+
+        ProvenanceLog.appendProvenanceLog(
+                new File(cachePath),
+                contentProvenanceWithNamespace
+        );
+        return blobStore.get(dereferenced);
+    }
+
+
+}
---
+++ src/test/java/org/globalbioticinteractions/elton/store/CachePullThroughPrestonStoreTest.java
@@ -0,0 +1,67 @@
+package org.globalbioticinteractions.elton.store;
+
+import bio.guoda.preston.store.BlobStoreAppendOnly;
+import bio.guoda.preston.store.KeyTo1LevelPath;
+import bio.guoda.preston.store.KeyValueStoreLocalFileSystem;
+import org.apache.commons.rdf.api.IRI;
+import org.apache.tika.io.IOUtils;
+import org.globalbioticinteractions.cache.Cache;
+import org.hamcrest.core.Is;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+
+public class CachePullThroughPrestonStoreTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+
+    @Test
+    public void testPrestonStore() throws IOException, URISyntaxException {
+
+        Cache cache = new CachePullThroughPrestonStore(
+                "some/namespace"
+                , folder.getRoot().getAbsolutePath()
+                , in -> in);
+
+        File namespaceDir = new File(folder.getRoot(), "some/namespace");
+        assertFalse(new File(namespaceDir, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").exists());
+
+        InputStream is = cache.retrieve(getClass().getResource("hello.txt").toURI());
+        assertTrue(new File(namespaceDir, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").exists());
+
+        assertThat(IOUtils.toString(is, StandardCharsets.UTF_8.name()), Is.is("hello"));
+
+
+        assertFalse(new File(folder.getRoot(), "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").exists());
+
+        BlobStoreAppendOnly blobStore = new BlobStoreAppendOnly(new KeyValueStoreLocalFileSystem(
+                folder.getRoot(),
+                new KeyTo1LevelPath(folder.getRoot().toURI()),
+                new KeyValueStoreLocalFileSystem.KeyValueStreamFactorySHA256Values()));
+
+        assertFalse(new File(folder.getRoot(), "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").exists());
+
+        IRI hash = blobStore.put(IOUtils.toInputStream("hello", StandardCharsets.UTF_8.name()));
+
+        assertThat(hash.getIRIString(), Is.is("hash://sha256/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"));
+
+        InputStream inputStream = blobStore.get(hash);
+
+        assertThat(IOUtils.toString(inputStream, StandardCharsets.UTF_8.name()), Is.is("hello"));
+
+        assertTrue(new File(folder.getRoot(), "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").exists());
+    }
+
+}

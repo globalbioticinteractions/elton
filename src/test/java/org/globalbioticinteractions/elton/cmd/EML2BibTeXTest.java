3ca1f39dd90283b67443b166a6f6080e847d67db
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-01-12 18:22:44 -0600

    add keywords to BibTeX snippets

--- src/test/java/org/globalbioticinteractions/elton/cmd/EML2BibTeXTest.java
+++ src/test/java/org/globalbioticinteractions/elton/cmd/EML2BibTeXTest.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eol.globi.taxon.XmlUtil;
 import org.eol.globi.util.DateUtil;
-import org.eol.globi.util.ResourceUtil;
 import org.hamcrest.core.Is;
 import org.jbibtex.BibTeXDatabase;
 import org.jbibtex.BibTeXEntry;
@@ -42,6 +41,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class EML2BibTeXTest {
 
     public static final Key ABSTRACT = new Key("abstract");
+    public static final Key KEYWORDS = new Key("keywords");
 
     @Test
     public void transformDruckerToBibTex() throws TransformerException, URISyntaxException, IOException, XPathExpressionException, SAXException, ParserConfigurationException {
@@ -54,6 +54,7 @@ public class EML2BibTeXTest {
         assertThat(entry.getField(BibTeXEntry.KEY_TITLE).toUserString(), Is.is("Abundância e Distribuição de Ervas Terrestres em Parcelas Ripárias na Reserva Ducke: Variação Lateral"));
         assertThat(entry.getField(BibTeXEntry.KEY_PUBLISHER).toUserString(), Is.is("Instituto Nacional de Pesquisas da Amazônia – INPA"));
         assertThat(entry.getField(ABSTRACT).toUserString(), Is.is("Os dados aqui disponibilizados são produto do trabalho realizado por Debora Drucker durante seu curso de mestrado. O objetivo central foi investigar a abundância e distribuição espacial de ervas terrestres (apenas as espécies que germinam e passam todo o seu ciclo de vida no solo, sensu Poulsen (1996)) em 20 Parcelas ripárias paralelas aos igarapés na Reserva Florestal Adolpho Ducke. Referência: Poulsen, A. D. 1996. Species richness and density of ground herbs within a plot of lowland rainforest in north-west Borneo. Journal of Tropical Ecology 12: 177-190."));
+        assertThat(entry.getField(KEYWORDS).toUserString(), Is.is("Ervas, Parcelas Ripárias, Reserva Ducke, Floresta de Terra Firme, PELD-PPBio"));
     }
 
     @Test
@@ -69,7 +70,6 @@ public class EML2BibTeXTest {
         assertThat(entry.getField(BibTeXEntry.KEY_PUBLISHER).toUserString(), Is.is("Ecdysis Portal"));
         assertThat(entry.getField(ABSTRACT).toUserString(), Is.is("University of California Santa Barbara Invertebrate Zoology Collection, Cheadle Center for Biodiversity and Ecological Restoration. Contributions to data in this collection come from Elaine Tan (https://orcid.org/0000-0002-0504-4067), Rachel Behm (https://orcid.org/0000-0001-7264-3492) and Zach Brown. The data is archived at https://doi.org/10.5281/zenodo.5660088."));
         assertThat(entry.getField(BibTeXEntry.KEY_URL).toUserString(), Is.is("https://ecdysis.org/collections/misc/collprofiles.php?collid=38"));
-
 
 
         BibTeXDatabase database = new BibTeXDatabase();
@@ -97,6 +97,7 @@ public class EML2BibTeXTest {
         assertThat(entry.getField(BibTeXEntry.KEY_TITLE).toUserString(), Is.is("WorldFAIR pilot data from: VisitationData_Luisa_Carvalheiro"));
         assertThat(entry.getField(BibTeXEntry.KEY_PUBLISHER).toUserString(), Is.is("University of Goias and University of Sao Paulo"));
         assertThat(entry.getField(ABSTRACT).toUserString(), Is.is("Note: This is an example of the data included in a large database of data on plant-flower visitor interactions, where datasets were only inlcuded when quantitative information on visitation rate is available at species level, and inofmration on flower anundance and sampling effort is available for each flowering species present in the study site."));
+        assertThat(entry.getField(KEYWORDS).toUserString(), Is.is("plant-pollinator interactions, visitation data, worldfair"));
     }
 
     private void populateDatasetCitation(InputStream is, BibTeXEntry entry) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
@@ -104,6 +105,19 @@ public class EML2BibTeXTest {
         addCreators(entry, dataset);
         addPublisher(entry, dataset);
         addTitle(entry, dataset);
+        NodeList keywords = (NodeList) XmlUtil.applyXPath(dataset, "keywordSet/keyword", XPathConstants.NODESET);
+        List<String> keywordList = new ArrayList<>();
+        for (int i = 0; i < keywords.getLength(); i++) {
+            Node item = keywords.item(i);
+            String keywordString = StringUtils.trim(item.getTextContent());
+            if (!keywordList.contains(keywordString)) {
+                keywordList.add(keywordString);
+            }
+        }
+        if (keywordList.size() > 0) {
+            entry.addField(KEYWORDS, new StringValue(StringUtils.join(keywordList, ", "), StringValue.Style.BRACED));
+        }
+
         addURL(entry, dataset);
         addAbstract(entry, dataset);
         addPubDate(entry, dataset);
@@ -116,7 +130,7 @@ public class EML2BibTeXTest {
                 new URI(urlStringCandidate);
                 entry.addField(BibTeXEntry.KEY_URL, new StringValue(StringUtils.trim(urlStringCandidate), StringValue.Style.BRACED));
             } catch (URISyntaxException e) {
-               //
+                //
             }
         }
     }

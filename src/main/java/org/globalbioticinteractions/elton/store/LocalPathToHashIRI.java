d7e23543d7fa4d77ae1017878f56e7770c68c7bc
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-02-20 12:40:23 -0600

    towards making classpath resource explicit; related to https://github.com/globalbioticinteractions/elton/issues/65

--- src/main/java/org/globalbioticinteractions/elton/store/LocalPathToHashIRI.java
+++ src/main/java/org/globalbioticinteractions/elton/store/LocalPathToHashIRI.java
@@ -12,12 +12,17 @@ import org.globalbioticinteractions.cache.ProvenanceLog;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LocalPathToHashIRI implements Dereferencer<IRI> {
 
     private final File dataFolder;
+
+    private final Pattern CLASSPATH_URL_PATTERN = Pattern.compile("([a-z]+:)*(classpath:)(?<classpath>[^!]+)(.*)");
+
 
     public LocalPathToHashIRI(File dataFolder) {
         this.dataFolder = dataFolder;
@@ -28,32 +33,54 @@ public class LocalPathToHashIRI implements Dereferencer<IRI> {
         URI original = URI.create(iri.getIRIString());
         URI translated = null;
         if (CacheUtil.isInCacheDir(dataFolder, original)) {
-            Matcher filePatternMatcher = CacheUtil.FILE_URL_PATTERN.matcher(iri.getIRIString());
-            if (filePatternMatcher.matches()) {
-                String filepath = filePatternMatcher.group("filepath");
-                Pattern hashPattern = Pattern.compile("(.*)(?<hash>[a-f0-9]{64})$");
-                Matcher matcher = hashPattern.matcher(filepath);
-                if (matcher.matches()) {
-                    String hash = matcher.group("hash");
-                    filepath = Hasher.toHashIRI(HashType.sha256, hash).getIRIString();
-                    String filePrefix = filePatternMatcher.group(2);
-                    String suffix = filePatternMatcher.group(4);
-                    String toBeReplaced =
-                            filePrefix
-                                    + filePatternMatcher.group("filepath")
-                                    + suffix;
+            translated = getLocationInDataDir(iri, original, translated);
+        } else {
+            translated = handleAsClasspath(iri);
+        }
+        return RefNodeFactory.toIRI(translated == null ? original : translated);
+    }
 
-                    String translatedString = StringUtils.replace(
-                            original.toString(),
-                            toBeReplaced,
-                            filepath + suffix
-                    );
-
-                    translated = URI.create(translatedString);
+    private URI handleAsClasspath(IRI iri) {
+        URI translated = null;
+        Matcher matcher = CLASSPATH_URL_PATTERN.matcher(iri.getIRIString());
+        if (matcher.matches()) {
+            URL location = getClass().getResource(matcher.group("classpath"));
+            if (location != null) {
+                try {
+                    translated = location.toURI();
+                } catch (URISyntaxException e) {
+                    // ignore
                 }
             }
         }
+        return translated;
+    }
 
-        return RefNodeFactory.toIRI(translated == null ? original : translated);
+    private URI getLocationInDataDir(IRI iri, URI original, URI translated) {
+        Matcher filePatternMatcher = CacheUtil.FILE_URL_PATTERN.matcher(iri.getIRIString());
+        if (filePatternMatcher.matches()) {
+            String filepath = filePatternMatcher.group("filepath");
+            Pattern hashPattern = Pattern.compile("(.*)(?<hash>[a-f0-9]{64})$");
+            Matcher matcher = hashPattern.matcher(filepath);
+            if (matcher.matches()) {
+                String hash = matcher.group("hash");
+                filepath = Hasher.toHashIRI(HashType.sha256, hash).getIRIString();
+                String filePrefix = filePatternMatcher.group(2);
+                String suffix = filePatternMatcher.group(4);
+                String toBeReplaced =
+                        filePrefix
+                                + filePatternMatcher.group("filepath")
+                                + suffix;
+
+                String translatedString = StringUtils.replace(
+                        original.toString(),
+                        toBeReplaced,
+                        filepath + suffix
+                );
+
+                translated = URI.create(translatedString);
+            }
+        }
+        return translated;
     }
 }
--- src/test/java/org/globalbioticinteractions/elton/store/LocalPathToHashIRITest.java
+++ src/test/java/org/globalbioticinteractions/elton/store/LocalPathToHashIRITest.java
@@ -12,9 +12,14 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class LocalPathToHashIRITest {
 
@@ -71,6 +76,29 @@ public class LocalPathToHashIRITest {
         );
 
         assertThat(iri.getIRIString(), Is.is("jar:hash://sha256/4df3c3f68fcc83b27e9d42c90431a72499f17875c81a599b566c9889b9696703!/foo"));
+    }
+
+    @Test
+    public void hashEmbeddedJarInClasspath() throws IOException {
+        File dataFolder = folder.newFolder();
+
+        String hashId = Hasher.calcHashIRI("hello", HashType.sha256).getIRIString();
+
+        String hex = StringUtils.substring(hashId, HashType.sha256.getPrefix().length());
+        File file = new File(dataFolder, hex);
+
+        assertThat(file.getAbsolutePath(), endsWith("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"));
+
+        LocalPathToHashIRI localPathToHashIRI = new LocalPathToHashIRI(dataFolder);
+
+        String stringClassPath = "classpath:/java/lang/String.class";
+        IRI iri = localPathToHashIRI.get(
+                RefNodeFactory.toIRI(
+                        stringClassPath)
+        );
+
+        assertThat(iri.getIRIString(), startsWith("jar:file:"));
+        assertThat(iri.getIRIString(), endsWith("!/java/lang/String.class"));
     }
 
     @Test

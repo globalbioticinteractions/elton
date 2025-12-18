1fd1c2db17f639719d79a7fa5520effad624926e
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-17 18:58:26 -0600

    remove unused travis config

--- .travis.deploy_if_tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -n "$TRAVIS_TAG" ]; then
-  mvn -s .travis.maven.settings.xml -DskipTests clean deploy
-  VERSION=${TRAVIS_TAG//[^0-9.]/}
-  cp target/elton-app.jar elton_no_head.jar
-  cat .travis.jar.magic elton_no_head.jar > elton.jar
-fi

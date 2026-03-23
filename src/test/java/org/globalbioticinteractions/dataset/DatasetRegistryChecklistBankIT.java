1830d22f5b88128756d312e71f3860640862a4a8
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-03-23 18:59:25 -0500

    work towards listing checklistbank; https://github.com/globalbioticinteractions/globalbioticinteractions/issues/753

---
+++ src/test/java/org/globalbioticinteractions/dataset/DatasetRegistryChecklistBankIT.java
@@ -0,0 +1,46 @@
+package org.globalbioticinteractions.dataset;
+
+import org.eol.globi.service.ResourceService;
+import org.eol.globi.util.InputStreamFactoryNoop;
+import org.eol.globi.util.ResourceServiceHTTP;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasItem;
+
+public class DatasetRegistryChecklistBankIT  {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void datasetIds() throws IOException, DatasetRegistryException {
+        ResourceService resourceService
+                = new ResourceServiceHTTP(new InputStreamFactoryNoop(), folder.newFolder());
+
+        DatasetRegistryChecklistBank checklistBank
+                = new DatasetRegistryChecklistBank(resourceService);
+
+        ArrayList<String> ids = new ArrayList<>();
+
+        checklistBank.findNamespaces(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                ids.add(s);
+            }
+        });
+
+        assertThat(ids.size(), greaterThan(0));
+
+        assertThat(ids, hasItem("urn:lsid:checklistbank.org:dataset:1049"));
+        assertThat(ids, hasItem("urn:lsid:checklistbank.org:dataset:2017"));
+    }
+
+}
--- src/test/java/org/globalbioticinteractions/dataset/DatasetRegistryChecklistBankTest.java
+++ src/test/java/org/globalbioticinteractions/dataset/DatasetRegistryChecklistBankTest.java
@@ -1,23 +1,44 @@
 package org.globalbioticinteractions.dataset;
 
+import org.eol.globi.service.ResourceService;
+import org.eol.globi.util.InputStreamFactoryNoop;
+import org.eol.globi.util.ResourceServiceHTTP;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 
 public class DatasetRegistryChecklistBankTest {
 
     @Test
+    public void listDatasets() throws DatasetRegistryException {
+        ResourceService resourceService
+                = new ResourceService() {
+            @Override
+            public InputStream retrieve(URI uri) throws IOException {
+                return getClass().getResourceAsStream("checklistbank-list-page1.json");
+            }
+        };
+
+        DatasetRegistryChecklistBank checklistBank
+                = new DatasetRegistryChecklistBank(resourceService);
+        Iterable<String> datasetIds = checklistBank.findNamespaces();
+
+        assertThat(datasetIds, hasItem("urn:lsid:checklistbank.org:dataset:1049"));
+    }
+
+    @Test
     public void datasetsPage1() throws DatasetRegistryException {
         InputStream resourceAsStream = getClass().getResourceAsStream("checklistbank-list-page1.json");
-        Collection<String> refs = DatasetRegistryChecklistBank.getRelations(resourceAsStream);
+        Collection<String> refs = DatasetRegistryChecklistBank.getDatasetIds(resourceAsStream);
         assertThat(refs.size(), is(5));
         assertThat(refs, hasItem("urn:lsid:checklistbank.org:dataset:1049"));
         assertThat(refs, not(hasItem("urn:lsid:checklistbank.org:dataset:2017")));
@@ -26,7 +47,7 @@ public class DatasetRegistryChecklistBankTest {
     @Test
     public void datasetsPage2() throws DatasetRegistryException {
         InputStream resourceAsStream = getClass().getResourceAsStream("checklistbank-list-page2.json");
-        Collection<String> refs = DatasetRegistryChecklistBank.getRelations(resourceAsStream);
+        Collection<String> refs = DatasetRegistryChecklistBank.getDatasetIds(resourceAsStream);
         assertThat(refs.size(), is(5));
         assertThat(refs, not(hasItem("urn:lsid:checklistbank.org:dataset:1049")));
         assertThat(refs, hasItem("urn:lsid:checklistbank.org:dataset:2017"));
@@ -35,7 +56,7 @@ public class DatasetRegistryChecklistBankTest {
     @Test
     public void datasetsPage3() throws DatasetRegistryException {
         InputStream resourceAsStream = getClass().getResourceAsStream("checklistbank-list-page3.json");
-        Collection<String> refs = DatasetRegistryChecklistBank.getRelations(resourceAsStream);
+        Collection<String> refs = DatasetRegistryChecklistBank.getDatasetIds(resourceAsStream);
         assertThat(refs.size(), is(1));
         assertThat(refs, hasItem("urn:lsid:checklistbank.org:dataset:265709"));
         assertThat(refs, not(hasItem("urn:lsid:checklistbank.org:dataset:2017")));
